d752024d86be52eb1f8dea2d87e50172212bb257
saivenkat-tech <psaivenkat101@gmail.com>
2025-05-19 11:37:37 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,29 @@
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const { Configuration, OpenAIApi } = require('openai');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+app.post('/ask', async (req, res) => {
+  const { question } = req.body;
+  try {
+    const completion = await openai.createChatCompletion({
+      model: 'gpt-3.5-turbo',
+      messages: [{ role: 'user', content: question }],
+    });
+    res.json({ response: completion.data.choices[0].message.content });
+  } catch (error) {
+    res.status(500).send('Error generating response');
+  }
+});
+
+app.listen(3001, () => console.log('Server running on port 3001'));
